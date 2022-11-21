fc541bfae2fd8c819f563853215b76ce106e0329
AlshainSC <nicholas.m.larson@gmail.com>
2022-11-21 18:31:27 +0100

    ..

--- src/contexts/CocktailProvider.tsx
+++ src/contexts/CocktailProvider.tsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 import { getAllCocktails } from '../services/services';
 import { Cocktail } from '../Types/Type';
-// Your Butt//butt
+//doublebutt
 interface Props {
   cocktails: Cocktail[];
   setCocktails: React.Dispatch<React.SetStateAction<Cocktail[]>>;
